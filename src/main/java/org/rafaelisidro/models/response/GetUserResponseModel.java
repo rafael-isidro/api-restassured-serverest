9acbc3af28654a8cc00afab31fe64309c2c9284b
rafael-isidro <rafa-isidro@hotmail.com>
2026-03-01 14:52:14 -0300

    feat: adding models and manipulation classes

---
+++ src/main/java/org/rafaelisidro/models/response/GetUserResponseModel.java
@@ -0,0 +1,18 @@
+package org.rafaelisidro.models.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetUserResponseModel {
+
+    private String nome;
+    private String email;
+    private String password;
+    private String administrador;
+    private String _id;
+
+}
--- src/main/java/org/rafaelisidro/utils/Manipulation.java
+++ src/main/java/org/rafaelisidro/utils/Manipulation.java
@@ -1,4 +1,22 @@
 package org.rafaelisidro.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Manipulation {
+
+    private Manipulation() {}
+
+    public static Properties getProp() {
+        Properties props = new Properties();
+
+        try (FileInputStream file = new FileInputStream("src/main/resources/application-test.properties")){
+            props.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        return props;
+    }
 }
